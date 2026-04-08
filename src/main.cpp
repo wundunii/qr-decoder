fc10b43d9d81725ff14f611361afb69f89404da3
wundunii <wundunii@proton.me>
2026-04-08 02:38:09 +0200

    Update to use quirc

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-#include <ReadBarcode.h>
+#include "quirc.h"
 
 int main(int argc, char* argv[]) {
   std::cout << "Initializing QR Decoder..." << std::endl;
@@ -16,13 +16,12 @@ int main(int argc, char* argv[]) {
 
   int width = 0;
   int height = 0;
-  int in_channels = 0;
-  int out_channels = 1; // 1 = Grayscale
+  int channels = 0;
 
   // unsigned char is 1 byte = a pixel's color value 0-255
-  unsigned char* data = stbi_load(argv[1], &width, &height, &in_channels, out_channels);
+  unsigned char* pixels = stbi_load(argv[1], &width, &height, &channels, STBI_grey);
 
-  if (data == nullptr) {
+  if (pixels == nullptr) {
     std::cerr << "Error: Could not load image.\n"
               << "Reason: " << stbi_failure_reason() << std::endl;
     return 2;
@@ -30,30 +29,39 @@ int main(int argc, char* argv[]) {
 
   std::cout << "Image successfully loaded.\n";
 
-  try {
-    //Create ImageView (throw if dimensions are invalid)
-    ZXing::ImageView img{data, width, height, ZXing::ImageFormat::Lum};
+  // Init quirc and alloc its internal memory
+  struct quirc* q = quirc_new();
+  quirc_resize(q, width, height);
 
-    //Decode (throw if mem alloc fails)
-    auto barcodes = ZXing::ReadBarcodes(img);
+  // Pointer to quirc's internal buffer
+  int q_width, q_height;
+  uint8_t* quirc_buffer = quirc_begin(q, &q_width, &q_height);
 
-    if (barcodes.empty()) {
-      std::cout << "Could not find QR code." << std::endl;
+  memcpy(quirc_buffer, pixels, width * height);
+
+  // Done loading data
+  quirc_end(q);
+
+  int res_count = quirc_count(q);
+  std::cout << "Found " << res_count << " QR codes." << std::endl;
+
+  for (int i = 0; i < res_count; i++) {
+    struct quirc_code code;
+    struct quirc_data data;
+    
+    quirc_extract(q, i, &code);
+
+    quirc_decode_error_t err = quirc_decode(&code, &data);
+
+    if (err != QUIRC_SUCCESS) {
+      std::cerr << "Decoding failed: " << quirc_strerror(err) << std::endl;
     } else {
-      std::cout << "-----------------------------\n" << "Result:" << std::endl;
-      for (const auto& barcode : barcodes) {
-        std::cout << barcode.text() << std::endl;
-      }
+      std::cout << "--------------------\nResult:\n" << data.payload << std::endl;
     }
   }
-  catch (const std::exception& e) {
-    std::cerr << "A fatal exception occurred during decoding: " << e.what() << std::endl;
-  }
-  catch (...) {
-    std::cerr << "An unknown error occurred!" << std::endl;
-  }
 
-  stbi_image_free(data);
+  quirc_destroy(q);
+  stbi_image_free(pixels);
 
   return 0;
 }
